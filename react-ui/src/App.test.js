62825e963c2c8ec6c95307b78e1d4de5cc31cf6a
Ratheesh <ratkumar@linkedin.com>
2020-12-21 13:10:53 +0530

    Test commit

---
+++ react-ui/src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function sum(a, b) {
+  return a + b;
+}
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
